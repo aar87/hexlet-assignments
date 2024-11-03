84f3388c2dfe287f185123de7ab206e0eca39bdf
@hexlet/cli <support@hexlet.io>
2024-11-03 18:14:50 +0300

    submit java-web-ru/handlers

--- java-web-ru/handlers/src/main/java/exercise/App.java
+++ java-web-ru/handlers/src/main/java/exercise/App.java
@@ -2,21 +2,14 @@ package exercise;
 
 import io.javalin.Javalin;
 
-import java.lang.reflect.Array;
-import java.util.List;
-
 public final class App {
 
     public static Javalin getApp() {
 
         // BEGIN
-
-        List<String> phone = List.of("1231233", "12313123123", "1312312323");
-        List<String> domains = List.of("d1231233", "d12313123123", "d1312312323");
-
         var app = Javalin.create();
-        app.get("/phones", ctx -> ctx.json(phone));
-        app.get("/domains", ctx -> ctx.json(domains));
+        app.get("/phones", ctx -> ctx.json(Data.getPhones()));
+        app.get("/domains", ctx -> ctx.json(Data.getPhones()));
 
         return app;
         // END
